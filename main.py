68103bcff2350b174c9403127895098d9a3c9a96
Aleksandra <tkaczyk.aleksandra@outlook.com>
2022-04-26 11:57:09 +0200

    shift characters on alphabet

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 
 
 def shift_characters(word, shift):
-    alphabet = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     word_list = []
     for letter in word:
-        letter_number = alphabet[letter]
-        if shift > 0:
-            new_letter_number = letter_number + shift
-            if letter_number > 26:
-                    
-            for key, value in alphabet.items():
-                if new_letter_number == value:
-                    letter = key
-            word_list.append(letter)
+        letter_number = alphabet.index(letter)
+        new_letter_number = letter_number + shift
+        print(new_letter_number)
+        if new_letter_number > len(alphabet) - 1 and new_letter_number <= 50:
+            new_letter_number -= 26
+            word_list.append(alphabet[new_letter_number])
+        if new_letter_number >= 51:
+            new_letter_number -= 52
+            word_list.append(alphabet[new_letter_number])
+        else:
+            word_list.append(alphabet[new_letter_number])
+    if len(word_list) > len(word):
+        word_list.pop()
+    return word_list
 
 
-shift_characters('ola', 1)
+print(shift_characters('ola', 50))
