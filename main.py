4948730bdaf309f369c1b7928cef636f2926bb34
Aleksandra <tkaczyk.aleksandra@outlook.com>
2022-04-27 12:56:50 +0200

    Extend a word with shifted variants, up to a given length

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
 
+from re import A
+
+
 def shift_characters(word, shift):
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     word_list = []
     for letter in word:
         letter_number = alphabet.index(letter)
         new_letter_number = letter_number + shift
-        print(new_letter_number)
         if new_letter_number > len(alphabet) - 1 and new_letter_number <= 50:
             new_letter_number -= 26
             word_list.append(alphabet[new_letter_number])
@@ -17,7 +19,22 @@ def shift_characters(word, shift):
             word_list.append(alphabet[new_letter_number])
     if len(word_list) > len(word):
         word_list.pop()
-    return word_list
+    return ''.join(word_list)
 
 
-print(shift_characters('ola', 50))
+
+def pad_up_to(word, shift, n):
+    word_list = []
+    word = word
+    while True:
+        if len(''.join(word_list)) <= n:
+            word = shift_characters(word, shift)
+            word_list.append(word)
+            continue
+        else:
+            break
+    word_list = ''.join(word_list)
+    return word_list[0: n]
+
+print(pad_up_to('aaa', 2, 10))
+
